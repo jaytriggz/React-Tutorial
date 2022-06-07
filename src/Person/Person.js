5ec76b9891a2d439c50a252d9a60e4043fcbaacd
jaytriggz <arotroy01@hotmail.com>
2022-06-07 13:58:47 +0100

    first commit on react

---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from "react" 
+
+const person = (props) => {
+    return (
+        <div>
+            <h2>My name is {props.name} i am {(Math.floor(Math.random()*34))} years old</h2>
+        </div>
+    )
+}
+
+export default person;
