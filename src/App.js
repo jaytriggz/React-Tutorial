b78dc35739d4712c8ded4f76c9b8aeab0b675553
jaytriggz <arotroy01@hotmail.com>
2022-06-14 11:31:19 +0100

    Updating React
    Phillz class is getting serious!!!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-
+import { Component } from 'react';
 import './App.css';
 import {render} from '@testing-library/react'
 import Triggzz from './Person/Person'
 
-function App() {
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'PLLILZ', age: '12'},
+      {name: 'ANU', age: '22'},
+      {name: 'joel', age: '32'},
+      {name: 'sekemi', age: '17'},
+    ]
+
+  }
+
+  switchNameHandler=()=>{
+    this.setState = {
+      persons:[
+        {name: "Timo", age: 2},
+        {name: "Waz", age: 22},
+        {name: "SemSem", age: 32},
+
+      ]
+    }
+  }
+  render(){
   return (
     <div className='App'>
-      <h1> Bola Ahmed Tinubu</h1>
-      <Triggzz name ="Morayo"/> <Triggzz name="Anu"/> <Triggzz name="Nonny"/>
+      <h1> Bola Ahmed Tinubu. BAT </h1>
+      
+      <Triggzz name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+      <Triggzz name = {this.state.persons[1].name } age ={this.state.persons[1].age} />
+      <Triggzz name = {this.state.persons[2].name } age ={this.state.persons[2].age} />
+
+      <button onClick={this.switchNameHandler}> SWITCH NAME </button>
+      
     </div>
   )
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@ import React from "react"
 const person = (props) => {
     return (
         <div>
-            <h2>My name is {props.name} i am {(Math.floor(Math.random()*34))} years old</h2>
+            <h2 onClick={props.clicked}>My name is {props.name} i am {(Math.floor(Math.random()*34))} years old</h2>
+
         </div>
     )
 }
